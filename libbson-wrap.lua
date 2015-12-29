bbf4236d59893226980a1d1304f4637772f95834
linbc <linbc01@gmail.com>
2015-12-29 23:04:41 +0800

    错误类型定义还是挺重要的，不能省

--- libbson-wrap.lua
+++ libbson-wrap.lua
@@ -31,7 +31,12 @@ typedef enum
 } bson_type_t;
 
 typedef struct _bson_oid_t bson_oid_t;
-typedef struct _bson_error_t bson_error_t;
+typedef struct _bson_error_t
+{
+   uint32_t domain;
+   uint32_t code;
+   char     message[504];
+} bson_error_t;
 typedef struct _bson_t bson_t;
 typedef struct {char __[128];} bson_iter_t;
 typedef long long time_t;
